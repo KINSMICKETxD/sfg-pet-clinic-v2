8e6869caa277d376b0051803dc22da55014a30c7
KINSMICKETxD <aboudaorg@gmail.com>
2023-03-30 13:55:33 +0100

    Initial Project add

---
+++ sfg-pet-clinic-v2/src/test/java/guru/springframework/sfgpetclinicv2/SfgPetClinicV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
